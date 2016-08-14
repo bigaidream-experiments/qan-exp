1e83a77ff7a8969634a8c515b001f22a44161082
jie <jie@next-gpu4.nus.edu.sg>
2016-08-14 23:31:50 +0800

    qan vs baseline.

--- torchnet_qan/20160815/paint.py
+++ torchnet_qan/20160815/paint.py
@@ -4,12 +4,19 @@ import matplotlib.pyplot as plt
 import sys
 
 fin = open('torchnet_test_baseline.log')
+fin1 = open('torchnet_test_loss.log')
 
 ar = []
 for i in fin:
     ar.append(i[:-1])
 
+ar1 = []
+for i in fin1:
+	ar1.append(i[:-1])
+
 ar = [float(x) for x in ar]
+ar1 = [float(x) for x in ar1]
+ar1 = ar1[0:200]
 ##baseline VS last episode
 
 t = range(0, len(ar))
@@ -17,7 +24,8 @@ t = range(0, len(ar))
 plt.title('')
 plt.ylabel('Accuracy(%)')
 plt.xlabel('Epoch')
-plt.plot(t, ar, label='qan')
+plt.plot(t, ar, label='baseline')
+plt.plot(t, ar1, label='qan')
 plt.legend(loc='lower right')
 
 plt.savefig('acc.pdf')
